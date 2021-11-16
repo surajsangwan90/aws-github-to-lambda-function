1f51aae0984d1c3bb144ee18752dee914eaaa8ba
Suraj <suraj.chaudhary58@gmail.com>
2021-11-16 10:16:27 +0530

    Testing Purpose for Deployment

--- pandas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-def lambda_handler(event,context):    
-    # list of strings
-    lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks']
-    # Calling DataFrame constructor on list
-    df = pd.DataFrame(lst)
-    print(df)
-    print("Successful Done")
